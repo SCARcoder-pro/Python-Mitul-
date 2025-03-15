e052bae1bf194649c2198b9a1e24117aaf755732
SCARcoder-pro <mitulkarna310@gmail.com>
2025-03-15 18:35:00 +0545

    initial commit

---
+++ a.py
@@ -0,0 +1,10 @@
+print("Welcome To Python Quiz!") 
+print("How many continents are there in the world?")
+print("1. Six")
+print("2. Seven")
+print("3. Eight")
+choice = input("Enter your choice: ")
+if choice == "2":
+    print("Correct Answer!")
+else:
+    print("Sorry! Wrong Answer!")
---
+++ quiz.py
@@ -0,0 +1,30 @@
+print("Welcome To General Knowledge Quiz!")
+print("What is the highest peak in the world?")
+print("1. Mount Everest")
+print("2. Mount Fuji")
+print("3. Mount Kilimanjaro")
+choice = input("Enter your choice: ")
+if choice == "1":
+    print("Correct Answer!")
+else:
+    print("Sorry! Wrong Answer!")
+
+print("What is the most famous sport in the world?")
+print("1. Cricket")
+print("2. Football")
+print("3. Basketball")
+choice = input("Enter your choice: ")
+if choice == "2":
+    print("Correct Answer!")
+else:
+    print("Sorry! Wrong Answer!")
+
+print("What is the most famous indoor game in the world?")
+print("1. Carrom")
+print("2. Ludo")
+print("3. Chess")
+choice = input("Enter your choice: ")
+if choice == "3":
+    print("Correct Answer!")
+else:
+    print("Sorry! Wrong Answer!")
