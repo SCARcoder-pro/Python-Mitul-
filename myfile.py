3d3c53b19b56367583a7425d26c11f03efc9afd5
SCARcoder-pro <mitulkarna310@gmail.com>
2025-03-16 19:58:01 +0545

    initial commit

---
+++ myfile.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Welcome to the world of Programming")
+
+#Print a number
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("Hello", 5)
+
+#End argument of print statement
+print("Hello to ", end=" * ")
