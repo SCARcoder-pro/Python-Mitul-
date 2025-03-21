394d325f3540b9ef1e51c13a1ca8905ac1a1c02a
SCARcoder-pro <mitulkarna310@gmail.com>
2025-03-21 19:45:27 +0545

    commit

---
+++ single.py
@@ -0,0 +1,28 @@
+#Print a sentence
+print("Welcome to the world of Programming")
+
+#Print a number
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("Hello", 5)
+
+#End argument of print statement
+print("Hello to ", end=" * ")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+name = input("Enter your name: ")
+print("\nHello", name, "\nWelcome to Codingal")
+
+import keyword
+print("Python Keywords are...\n")
+print(keyword.kwlist)
