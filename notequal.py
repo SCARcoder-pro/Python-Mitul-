fe2d20b1a548f991d5c768a9efdc1e51e4cf4a12
SCARcoder-pro <mitulkarna310@gmail.com>
2025-04-11 20:04:45 +0545

    commit

--- notequal.py
+++ notequal.py
@@ -1,28 +1,6 @@
-a = 10
-b = 12
-c = 12
-
-print(a != b)
-print(b != c)
-
-
-a = "python"
-b = "coding"
-
-if a != b :
-    print(a, 'and', b, 'are different.')
-
-
-a = 4
-b = 5
-
-if (a == 1) != (b == 5):
-    print('Hello')
-
-
-
-
-a = int(input("enter a number"))
-
-if a%2 != 0 :
-    print(a, "is not even number.")
+string = input("Please enter your own string: ")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+print("\n The Original string =", string)
+print("\n The Reversed string =", string2)
