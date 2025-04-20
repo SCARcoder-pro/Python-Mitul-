75f93a7eb91d948abd9d4aa6878d78be368bb40c
SCARcoder-pro <mitulkarna310@gmail.com>
2025-04-20 19:52:53 +0545

    commit

--- pj.py
+++ pj.py
@@ -1,8 +1,19 @@
-age = int(input("Enter your age: "))
-if age >= 10:
-    if age <= 20:
-        print("Your age is : ", age, "\n you are allowed to enter the class")
-    else:
-        print("Your age is : ", age, "\n you are not allowed to enter the class")
+Number = int(input(" Please Enter any Number: "))
+Sum = 0 
+Temp = Number
+
+while(Temp > 0): 
+	Factorial = 1 
+	i = 1
+	Reminder = Temp % 10
+	while(i <= Reminder):
+		Factorial = Factorial * i
+		i = i + 1
+	print("\n Factorial of %d = %d" %(Reminder, Factorial))
+	Sum = Sum + Factorial
+	Temp = Temp // 10
+print("\n Sum of Factorials of a Given Number %d = %d" %(Number, Sum))
+if (Sum == Number):
+	print(" %d is a Strong Number" %Number)
 else:
-    print("Your age is : ", age, "\n you are not eligible")
+	print(" %d is not a Strong Number" %Number)
