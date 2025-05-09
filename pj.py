7aa00774a235c5ebf827791745928ece07bca10a
SCARcoder-pro <mitulkarna310@gmail.com>
2025-05-09 19:59:43 +0545

    commit

--- pj.py
+++ pj.py
@@ -1,19 +1,13 @@
-Number = int(input(" Please Enter any Number: "))
-Sum = 0 
-Temp = Number
-
-while(Temp > 0): 
-	Factorial = 1 
-	i = 1
-	Reminder = Temp % 10
-	while(i <= Reminder):
-		Factorial = Factorial * i
-		i = i + 1
-	print("\n Factorial of %d = %d" %(Reminder, Factorial))
-	Sum = Sum + Factorial
-	Temp = Temp // 10
-print("\n Sum of Factorials of a Given Number %d = %d" %(Number, Sum))
-if (Sum == Number):
-	print(" %d is a Strong Number" %Number)
-else:
-	print(" %d is not a Strong Number" %Number)
+def employee(name):
+    print(name)
+    
+def salary(exp):
+    if exp >= 5:
+        return 3000000
+    elif exp >= 3:
+        return 1000000
+    else:
+        return 500000
+employee("Ram")
+a = salary(15)
+print("The salary of the employee is", a)
