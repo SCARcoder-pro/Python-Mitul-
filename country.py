f203cfcdca55b9880d0a67809e6f644b1bc9bbab
SCARcoder-pro <mitulkarna310@gmail.com>
2025-05-30 20:17:50 +0545

    commit

--- country.py
+++ country.py
@@ -1,6 +1,8 @@
-country_code = {'India', '0091'
-    'Australia', '0025',
-    'Nepal', '00977',}
+country_code = {
+    'India': '0091',
+    'Australia': '0025',
+    'Nepal': '00977',
+}
 
 print("Country Code for India")
 print(country_code.get('India', 'Not Found'))
