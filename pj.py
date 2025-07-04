df31104366feba68624b77ae3f3d3ebb705e4b71
SCARcoder-pro <mitulkarna310@gmail.com>
2025-07-04 18:58:29 +0545

    commit

--- pj.py
+++ pj.py
@@ -1,14 +1,31 @@
-def information():
-    print("learning python")
-    print("using vscode platform")
+class Cat:
 
-information()
-information()
-information()
-information()
-information()
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
 
-def add(num1, num2):
-    return num1 + num2
+	def info(self):
+		print(f"I am a cat. My name is {self.name}. I am {self.age} years old.")
 
-print(add(3, 5))
+	def make_sound(self):
+		print("Meow")
+
+class Dog:
+
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+
+	def info(self):
+		print(f"I am a dog. My name is {self.name}. I am {self.age} years old.")
+
+	def make_sound(self):
+		print("Bark")
+
+cat1 = Cat("Kitty", 2.5)
+dog1 = Dog("Fluffy", 4)
+
+for animal in (cat1, dog1):
+	animal.make_sound()
+	animal.info()
+	animal.make_sound()
