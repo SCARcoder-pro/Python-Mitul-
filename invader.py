9f0e7c6a63278f587e0de514c1ddc6896535ca10
SCARcoder-pro <mitulkarna310@gmail.com>
2025-07-25 20:07:59 +0545

    commit

--- invader.py
+++ invader.py
@@ -8,19 +8,22 @@ PLAYER_START_X = 370
 PLAYER_START_Y = 380
 ENEMY_START_Y_MIN = 50
 ENEMY_START_Y_MAX = 150
-ENEMY_SPEED_X = 4
-ENEMY_SPEED_Y = 40
-BULLET_SPEED_Y = 10
+ENEMY_SPEED_X = 1      # Even slower enemy speed
+ENEMY_SPEED_Y = 15     # Even slower enemy drop
+BULLET_SPEED_Y = 4     # Even slower bullet speed
+PLAYER_SPEED = 2       # Even slower player speed
 COLLISION_DISTANCE = 27
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background = pygame.image.load("istockphoto-1403514917-612x612.jpg")
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))  # Fit background
 pygame.display.set_caption("Space Invader")
 icon = pygame.image.load("images.jpeg")
 pygame.display.set_icon(icon)
 
 playerImg = pygame.image.load("pixel-art-illustration-rocket-pixelated-rocket-rocket-space-icon-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector-removebg-preview.png")
+playerImg = pygame.transform.scale(playerImg, (60, 60))
 playerX = PLAYER_START_X
 playerY = PLAYER_START_Y
 playerX_change = 0
@@ -33,13 +36,16 @@ enemyY_change = []
 num_of_enemies = 6
 
 for i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load('images1-removebg-preview.png'))
-    enemyX.append(random.randint(0, SCREEN_WIDTH - 64))
+    img = pygame.image.load('images1-removebg-preview.png')
+    img = pygame.transform.scale(img, (55, 55))
+    enemyImg.append(img)
+    enemyX.append(random.randint(0, SCREEN_WIDTH - 55))
     enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
     enemyX_change.append(ENEMY_SPEED_X)
     enemyY_change.append(ENEMY_SPEED_Y)
 
-bulletImg = pygame.image.load("pixel-art-illustration-rocket-pixelated-rocket-rocket-space-icon-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector-removebg-preview.png")
+bulletImg = pygame.image.load("pixel-art-bullet-pixelated-bullet-bullet-pistol-weapon-icons-background-pixelated-for-the-pixel-art-game-and-icon-for-website-and-video-game-old-school-retro-vector-removebg-preview.png")
+bulletImg = pygame.transform.scale(bulletImg, (18, 36))
 bulletX = 0
 bulletY = PLAYER_START_Y
 bulletX_change = 0
@@ -70,7 +76,7 @@ def enemy(x, y, i):
 def fire_bullet(x, y):
     global bullet_state
     bullet_state = "fire"
-    screen.blit(bulletImg, (x + 16, y + 10))
+    screen.blit(bulletImg, (x + 21, y + 8))
 
 def isCollision(enemyX, enemyY, bulletX, bulletY):
     distance = math.sqrt((enemyX - bulletX) ** 2 + (enemyY - bulletY) ** 2)
@@ -87,9 +93,9 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -5
+                playerX_change = -PLAYER_SPEED
             if event.key == pygame.K_RIGHT:
-                playerX_change = 5
+                playerX_change = PLAYER_SPEED
             if event.key == pygame.K_SPACE and bullet_state == "ready":
                 bulletX = playerX
                 fire_bullet(bulletX, bulletY)
@@ -97,7 +103,7 @@ while running:
             playerX_change = 0
 
     playerX += playerX_change
-    playerX = max(0, min(playerX, SCREEN_WIDTH - 64))
+    playerX = max(0, min(playerX, SCREEN_WIDTH - 60))
 
     for i in range(num_of_enemies):
         if enemyY[i] > 340:
@@ -107,7 +113,7 @@ while running:
             break
     
         enemyX[i] += enemyX_change[i]
-        if enemyX[i] <= 0 or enemyX[i] >= SCREEN_WIDTH - 64:
+        if enemyX[i] <= 0 or enemyX[i] >= SCREEN_WIDTH - 55:
             enemyX_change[i] *= -1
             enemyY[i] += enemyY_change[i]
 
@@ -115,7 +121,7 @@ while running:
             bulletY = PLAYER_START_Y
             bullet_state = "ready"
             score_value += 1
-            enemyX[i] = random.randint(0, SCREEN_WIDTH - 64)
+            enemyX[i] = random.randint(0, SCREEN_WIDTH - 55)
             enemyY[i] = random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX)
 
         enemy(enemyX[i], enemyY[i], i)
