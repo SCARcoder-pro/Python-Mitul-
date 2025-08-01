8ee2db3959491dbd904e6dec814807a81dec6c89
SCARcoder-pro <mitulkarna310@gmail.com>
2025-08-01 20:01:30 +0545

    commit

--- num.py
+++ num.py
@@ -1,8 +1,22 @@
-import math
-print(math.sin(45))
-print(math.cos(45))
-print(math.tan(45))
+from tkinter import *
+root = Tk()
+root.title('Number Pad')
+root.geometry('250x300')
+nums = [[9, 8, 7], [6, 5, 4], [3, 2, 1], ['#', 0, '*']]
+for i in range(4):
+    root.columnconfigure(i, weight=1, minsize = 75)
+    root.rowconfigure(i, weight=1, minsize = 50)
 
-print(math.ceil(45.490))
-print(math.floor(45.890))
-print(math.factorial(5))
+for i in range(4):
+    for j in range(3):
+        frame = Frame(
+            master=root,
+            relief=SUNKEN,
+            borderwidth=1,
+            bg='#d0efff'
+        )
+        frame.grid(row=i, column=j, sticky='nsew')
+        label = Label(master=frame, text=nums[i][j], bg='#d0efff', font=('Arial', 19))
+        label.pack(expand=True, fill='both', padx=5, pady=5)
+
+root.mainloop()
