df51006940a67b672aebc8bf5378ac1e6e6f1407
SCARcoder-pro <mitulkarna310@gmail.com>
2025-11-11 22:18:30 +0545

    commit

--- b.py
+++ b.py
@@ -1,9 +1,13 @@
-x = 5
-y = "John"
-print(x)
-print(y)
+import random
+def pick_shirt_experiment():
+    shirts = ['black', 'black', 'black', 'white']
+    result = random.choice(shirts)
+    pro = shirts.count('white') /len(shirts)
+    print("Probability of picking a white shirt:", pro)
+    if result == 'white':
+        print("White shirt was picked.")
+    else:
+        print("Better luck next time.")
 
-codingal = "codingal"
-print(codingal)
-name = input("Enter your name: ")
-print("\nHello", name, "\nWelcome to Codingal")
+res = pick_shirt_experiment()
+print(res)
--- c.py
+++ c.py
@@ -1,3 +1,14 @@
-import keyword
-print("Python Keywords are...\n")
-print(keyword.kwlist)
+import random
+def pick_ball_experiment():
+    balls = ['red', 'green', 'blue']
+    result = random.choice(balls)
+    pro = balls.count('red') / len(balls)
+    print("Probability of picking a red ball:", pro)
+
+    if result == 'red':
+        print("Red ball was picked.")
+    else:
+        print("Better luck next time.")
+
+res = pick_ball_experiment()
+print(res)
