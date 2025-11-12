b697537b2e12148648a0d8c256d02a99cf0bbcaa
SCARcoder-pro <mitulkarna310@gmail.com>
2025-11-12 20:08:39 +0545

    commit

--- 3.py
+++ 3.py
@@ -1,5 +1,13 @@
-n=int(input("Enter the number: "))
-if(n%2==0):
-    print("The number is even")
-else:
-    print("The number is odd")
+def prob_a_or_b(a, b, all_possible_outcomes):
+    prob_a = len(a)/len(all_possible_outcomes)
+    prob_b = len(b)/len(all_possible_outcomes)
+    inter = a.intersection(b)
+    prob_enter = len(inter)/len(all_possible_outcomes)
+    return(prob_a + prob_b - prob_enter)
+
+events= {2, 4, 6}
+greater_than_two = {3, 4, 5, 6}
+all_possible_rolls = {1, 2, 3, 4, 5, 6}
+
+print("Probablity of rolling a number greater than 2")
+print(prob_a_or_b(events, greater_than_two, all_possible_rolls))
