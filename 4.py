67e9864ec74f2670211213021ede844fe171befd
SCARcoder-pro <mitulkarna310@gmail.com>
2025-11-17 18:26:14 +0545

    commit

--- 4.py
+++ 4.py
@@ -1,13 +1,13 @@
 import random
-def pick_dice_experiment():
-    dice = ['1', '2', '3', '4', '5', '6']
+def roll_dice_experiment():
+    dice = [1, 2, 3, 4, 5, 6]
     result = random.choice(dice)
-    pro = dice.count('6') /len(dice)
-    print("Probability of getting a six:", pro)
-    if result == '6':
-        print("Congrats! You got 6!")
+    pro = dice.count(6) / len(dice)
+    print("Probability of rolling a 6:", pro)
+    if result == 6:
+        print("Congrats, You rolled a 6!")
     else:
         print("Better luck next time.")
 
-res = pick_dice_experiment()
+res = roll_dice_experiment()
 print(res)
--- a.py
+++ a.py
@@ -1,10 +1,48 @@
-print("Welcome To Python Quiz!") 
-print("How many continents are there in the world?")
-print("1. Six")
-print("2. Seven")
-print("3. Eight")
-choice = input("Enter your choice: ")
-if choice == "2":
-    print("Correct Answer!")
-else:
-    print("Sorry! Wrong Answer!")
+def guess_the_type(event):
+  
+    print(
+        "Guess whether this is a \n 1)dependent \n 2)independent \n 3)mutually exclusive \n 4)not mutually exclusive event \n"
+    )
+    print(event)
+    answer = int(input("Enter your answer : "))
+    if event == Dependence:
+        if answer == 1:
+            print("\n You guessed it right! \n \n \n")
+            return 'correct'
+        else:
+            print("\n Your answer is wrong... \n \n \n")
+            return 'wrong'
+    if event == mutually_exclusive:
+        if answer == 3:
+            print("\n You guessed it right! \n \n \n")
+            return 'correct'
+        else:
+            print("\n Your answer is wrong... \n \n \n")
+            return 'wrong'
+    if event == notmutuallyexclusive:
+        if answer == 4:
+            print("\n You guessed it right! \n \n \n")
+            return 'correct'
+        else:
+            print("\n Your answer is wrong... \n \n \n")
+            return 'wrong'
+    if event == indipendent:
+        if answer == 2:
+            print("\n You guessed it right! \n \n \n")
+            return 'correct'
+
+        else:
+            print("\n Your answer is wrong... \n \n \n")
+            return 'wrong'
+
+Dependence = "Dependent Event"
+mutually_exclusive = "Mutually Exclusive Event"
+notmutuallyexclusive = "Not Mutually Exclusive Event"
+indipendent = "Independent Event"
+
+firstanswer=guess_the_type(Dependence)
+secondanswer=guess_the_type(mutually_exclusive)
+thirdanswer=guess_the_type(notmutuallyexclusive)
+fourthanswer=guess_the_type(indipendent)
+if firstanswer=='correct' and secondanswer=='correct' and thirdanswer=='correct' and fourthanswer=='correct':
+      print("\n \n \n You have guessed all the events correctly! Here's some cookies: 🍪🍪🍪 \n \n \n")
